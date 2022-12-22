c17023e1a0ab850084d178f4e464d03ddc2913da
Madison Lester <madiali@ad.unc.edu>
2022-12-22 10:55:30 -0500

    Keyboard

---
+++ MIPS_Emulator/Keyboard.java
@@ -0,0 +1,31 @@
+package MIPS_Emulator;
+
+public class Keyboard implements MemoryUnit {
+    private int keyCode = 0;
+
+    public Keyboard() {
+        this(4, 4);
+    }
+
+    public Keyboard(int size, int wordSize) {}
+
+    @Override
+    public int getMemoryUnit(int index) {
+        return keyCode;
+    }
+
+    @Override
+    public void setMemoryUnit(int index, int value) {
+        keyCode = value;
+    }
+
+    @Override
+    public int getSize() {
+        return 4;
+    }
+
+    @Override
+    public int getWordSize() {
+        return 4;
+    }
+}
