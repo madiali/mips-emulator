569fad39fdf542b679f0a9c834b3d1975c865281
Madison Lester <madiali@ad.unc.edu>
2022-12-23 12:00:20 -0500

    SltuInstruction bug fix

--- src/main/java/mips/instructions/RType/SltuInstruction.java
+++ src/main/java/mips/instructions/RType/SltuInstruction.java
@@ -12,7 +12,18 @@ public class SltuInstruction extends RTypeInstruction {
 
   @Override
   public void execute(ProgramCounter pc, MemoryMapper mem, Registers reg) {
-    boolean isLessThan = reg.getRegister(s) < reg.getRegister(t);
+    int sUpperBit = reg.getRegister(s) >>> 31;
+    int rUpperBit = reg.getRegister(t) >>> 31;
+    boolean isLessThan;
+    if (sUpperBit == 1 && rUpperBit == 0) {
+      isLessThan = false;
+    }
+    else if (sUpperBit == 0 && rUpperBit == 1) {
+      isLessThan = true;
+    }
+    else {
+      isLessThan = reg.getRegister(s) < reg.getRegister(t);
+    }
     reg.setRegister(d, (isLessThan ? 1 : 0));
     pc.incrementPC(4);
   }
