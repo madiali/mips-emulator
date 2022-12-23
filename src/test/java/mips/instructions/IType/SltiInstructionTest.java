54741d9d47e4268c75b0e39d2459fed38c8cf9b4
Jesse Wei <jessew13@email.unc.edu>
2022-12-23 12:08:40 -0500

    Add some more IType tests

---
+++ src/test/java/mips/instructions/IType/SltiInstructionTest.java
@@ -0,0 +1,43 @@
+package mips.instructions.IType;
+
+import mips.MemoryMapper;
+import mips.ProgramCounter;
+import mips.Registers;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SltiInstructionTest {
+    private ProgramCounter pc = new ProgramCounter(0x00000000);
+    private Registers reg = new Registers();
+    private MemoryMapper mem = new MemoryMapper(8);
+    private SltiInstruction target;
+
+    @Test
+    public void executeLessThanImmediate() {
+        reg.setRegister(8, 0x00000000);
+        target = new SltiInstruction(9, 8, 0x0001);
+        target.execute(pc, mem, reg);
+        assertEquals(0x00000001, reg.getRegister(9));
+        assertEquals(0x00000004, pc.getPC());
+    }
+
+    @Test
+    public void executeLessThanImmediateSignedComparison() {
+        reg.setRegister(8, 0xFFFFFFFF);
+        target = new SltiInstruction(9, 8, 0x0000);
+        target.execute(pc, mem, reg);
+        // Not sure why there's trailing 0's here
+        assertEquals(0x00000001, reg.getRegister(9));
+        assertEquals(0x00000004, pc.getPC());
+    }
+
+    @Test
+    public void executeNotLessThanImmediate() {
+        reg.setRegister(8, 0x00000001);
+        target = new SltiInstruction(9, 8, 0x0000);
+        target.execute(pc, mem, reg);
+        assertEquals(0x00000000, reg.getRegister(9));
+        assertEquals(0x00000004, pc.getPC());
+    }
+}
