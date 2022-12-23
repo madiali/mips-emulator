63ec6c1ac5b3ca84b34d1958bc2fe83320294f19
Madison Lester <madiali@ad.unc.edu>
2022-12-23 11:24:09 -0500

    More IType tests

---
+++ src/test/java/mips/instructions/IType/BeqInstructionTest.java
@@ -0,0 +1,31 @@
+package mips.instructions.IType;
+
+import mips.MemoryMapper;
+import mips.ProgramCounter;
+import mips.Registers;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BeqInstructionTest {
+    private ProgramCounter pc = new ProgramCounter(0x00000000);
+    private Registers reg = new Registers();
+    private MemoryMapper mem = new MemoryMapper(8);
+    private BeqInstruction target;
+
+    @Test
+    public void executeNoBranch() {
+        reg.setRegister(1, 1);
+        target = new BeqInstruction(1, 0, 0xFFFF);
+        target.execute(pc, mem, reg);
+        assertEquals(0x00000004, pc.getPC());
+    }
+
+	@Test
+    public void executeWithBranch() {
+        reg.setRegister(1, 0);
+        target = new BeqInstruction(1, 0, 0x0002);
+        target.execute(pc, mem, reg);
+        assertEquals(0x0000000C, pc.getPC());
+    }
+}
