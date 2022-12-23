c95c0c74eca9588ef8d6a22a4f2708387f03f520
Jesse Wei <jessew13@email.unc.edu>
2022-12-23 12:03:54 -0500

    Add some IType tests

---
+++ src/test/java/mips/instructions/IType/SwInstructionTest.java
@@ -0,0 +1,24 @@
+package mips.instructions.IType;
+
+import mips.MemoryMapper;
+import mips.ProgramCounter;
+import mips.Registers;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SwInstructionTest {
+    private ProgramCounter pc = new ProgramCounter(0x00000000);
+    private Registers reg = new Registers();
+    private MemoryMapper mem = new MemoryMapper(8);
+    private SwInstruction target;
+
+    @Test
+    public void executeDataStoredInMem() {
+        reg.setRegister(8, 0x11037);
+        target = new SwInstruction(8, 9, 0x0004);
+        target.execute(pc, mem, reg);
+        assertEquals(0x11037, mem.getMemoryUnit(4));
+        assertEquals(0x00000004, pc.getPC());
+    }
+}
