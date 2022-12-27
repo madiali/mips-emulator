cffc053d9306b4f2f52f6b5ca88cda7eceb283f4
Jesse Wei <jessew13@email.unc.edu>
2022-12-27 09:20:08 -0500

    Images can be resized, phew

--- src/main/java/GUI/HelloFX.java
+++ src/main/java/GUI/HelloFX.java
@@ -16,13 +16,33 @@ public class HelloFX extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         stage.setTitle("Hi :)");
+        // Hello there (:
         GridPane grid = new GridPane();
         for (int i = 0; i < 6; i++) {
             for (int j = 0; j < 6; j++) {
-                Image img = new Image("File:img/" + Integer.toHexString(5 * i + j) + ".bmp");
-                grid.add(new ImageView(img), j, i);
-            }
+                Image img = new Image("File:img/" + Integer.toHexString(6 * i + j) + ".bmp");
+                ImageView ogImg = new ImageView(img);
+                grid.add(ogImg, j, i);
 
+                ImageView bigImg = new ImageView(img);
+                bigImg.setFitHeight(32);
+                bigImg.setFitWidth(32);
+                bigImg.setPreserveRatio(true);
+                grid.add(bigImg, j, i + 6);
+
+                ImageView biggerImg = new ImageView(img);
+                biggerImg.setFitHeight(64);
+                biggerImg.setFitWidth(64);
+                biggerImg.setPreserveRatio(true);
+                grid.add(biggerImg, j, i + 12);
+
+                ImageView biggestImg = new ImageView(img);
+                biggestImg.setFitHeight(128);
+                biggestImg.setFitWidth(128);
+                biggestImg.setPreserveRatio(true);
+                grid.add(biggestImg, j, i + 18);
+
+            }
         }
         Scene scene = new Scene(grid);
         stage.setScene(scene);
