38a41054d4fe59942b90ef5e04c0b54ea420bb8f
Madison Lester <madiali@ad.unc.edu>
2022-12-27 20:34:46 -0500

    Add ViewTabs FXComponent

--- src/main/java/GUI/View.java
+++ src/main/java/GUI/View.java
@@ -34,12 +34,10 @@ public class View extends Application {
         main.setCenter(lhs);
 
         // Layout RHS center (register / memory)
-        Rectangle tempRectangle = new Rectangle(500, 700, Color.GRAY);
-        Insets rectangleInset = new Insets(10, 10, 10, 10);
-        StackPane rectangleStackPane = new StackPane();
-        rectangleStackPane.getChildren().add(tempRectangle);
-        rectangleStackPane.setPadding(rectangleInset);
-        main.setRight(tempRectangle);
+        ViewTabs viewTabs = new ViewTabs();
+        main.setRight(viewTabs.render());
+
+        // Center
         layout.setCenter(main);
 
         // Initialize scene
