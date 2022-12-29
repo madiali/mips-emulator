c593bf83f541db9d65d7697ea1c22ded457cff91
Jesse Wei <jessew13@email.unc.edu>
2022-12-29 16:22:26 -0500

    Get MIPS memory accelerometers in AccelerometerController

--- notes/skipped-files.md
+++ notes/skipped-files.md
@@ -6,6 +6,5 @@ There's also files that are incomplete as they depend on other files.
 * `Sound`
 * `SoundModule`
 * `SoundWaveGenerator`
-* `ProgramLoader`
-  * Once Sound is implemented, there needs to be a small change in this file
-  * And Sound also needs to go in MemoryUnitFactory when implemented, do not forget about this
+* `AccelerometerController`
+  * Handle the case when the accelerometer isn't in the JSON
--- src/main/java/controller/AccelerometerController.java
+++ src/main/java/controller/AccelerometerController.java
@@ -1,56 +1,78 @@
 package controller;
 
+import javafx.scene.Parent;
 import javafx.scene.control.Slider;
-import mips.AccelerometerX;
-import mips.AccelerometerY;
-import mips.Mips;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import mips.*;
 
 public class AccelerometerController implements DebuggerView {
-    private AccelerometerX accelerometerX;
-    private AccelerometerY accelerometerY;
-    private Slider xSlider;
-    private Slider ySlider;
-    private final double defaultAccelValue = 255;
+  private AccelerometerX accelerometerX;
+  private AccelerometerY accelerometerY;
+  private Slider xSlider;
+  private Slider ySlider;
+  private final double defaultAccelValue = 255;
 
-    /* The AccelerometerController might need to take mips as a parameter to get accelerometerX and accelerometerY from it? */
-    public AccelerometerController(Mips mips) {
-        // This is just for testing, later we'll make the Accelerometer objects the same as the ones in the Mips object
-        accelerometerX = new AccelerometerX();
-        accelerometerY = new AccelerometerY();
-        accelerometerX.setXValue((int) defaultAccelValue);
-        accelerometerY.setYValue((int) defaultAccelValue);
+  /* The AccelerometerController might need to take mips as a parameter to get accelerometerX and accelerometerY from the same Mips as the one in MipsController? */
+  public AccelerometerController(Mips mips) {
+    Accelerometer mappedAccelerometer = null;
+    for (MappedMemoryUnit mappedMemoryUnit : mips.getMemory().getMemUnits()) {
+      if (mappedMemoryUnit.getMemUnit() instanceof Accelerometer) {
+        mappedAccelerometer = (Accelerometer) mappedMemoryUnit.getMemUnit();
+        break;
+      }
     }
-
-    public void setXSlider(Slider xSlider) {
-        this.xSlider = xSlider;
+    if (mappedAccelerometer != null) {
+      this.accelerometerX = mappedAccelerometer.getAccelX();
+      this.accelerometerY = mappedAccelerometer.getAccelY();
+      accelerometerX.setXValue((int) defaultAccelValue);
+      accelerometerY.setYValue((int) defaultAccelValue);
+    } else {
+      close();
     }
+    // mappedAccelerometer will be null if Accelerometer is not in the JSON. We can add logic to
+    // handle this case later, which could include removing the Slider components since any action
+    // on a slider with null Accelerometer will throw Exception (which doesn't seem to crash the
+    // program?)
+  }
 
-    public void setYSlider(Slider ySlider) {
-        this.ySlider = ySlider;
-    }
+  public void setXSlider(Slider xSlider) {
+    this.xSlider = xSlider;
+  }
 
-    public void handleXSliderDrag() {
-        accelerometerX.setXValue((int) xSlider.getValue());
-    }
+  public void setYSlider(Slider ySlider) {
+    this.ySlider = ySlider;
+  }
 
-    public void handleYSliderDrag() {
-        accelerometerY.setYValue((int) ySlider.getValue());
-    }
+  public void handleXSliderDrag() {
+    accelerometerX.setXValue((int) xSlider.getValue());
+  }
 
-    public void handleResetButton() {
-        xSlider.setValue(defaultAccelValue);
-        ySlider.setValue(defaultAccelValue);
-        accelerometerX.setXValue((int) defaultAccelValue);
-        accelerometerY.setYValue((int) defaultAccelValue);
-    }
+  public void handleYSliderDrag() {
+    accelerometerY.setYValue((int) ySlider.getValue());
+  }
 
-    @Override
-    public void refreshDisplay() {
+  public void handleResetButton() {
+    xSlider.setValue(defaultAccelValue);
+    ySlider.setValue(defaultAccelValue);
+    accelerometerX.setXValue((int) defaultAccelValue);
+    accelerometerY.setYValue((int) defaultAccelValue);
+  }
 
-    }
+  /** Unnecessary to call this, sliders update on screen automatically. */
+  @Deprecated
+  @Override
+  public void refreshDisplay() {}
 
-    @Override
-    public void close() {
-
-    }
+  /**
+   * Call this to remove Sliders since Accelerometer is not mapped. However, this code fails when
+   * Accelerometer is not mapped, which somehow causes xSlider to be null???
+   */
+  @Override
+  public void close() {
+//        HBox parent = (HBox) MipsController.xSlider.getParent();
+//        parent.getChildren().remove(xSlider);
+//        parent = (HBox) MipsController.ySlider.getParent();
+//        parent.getChildren().remove(ySlider);
+  }
 }
