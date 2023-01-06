765d3f80f80145cbee3cf9ccc258eb8d2c933140
Madison Lester <madiali@ad.unc.edu>
2023-01-05 20:14:52 -0500

    Remove GUI temp folder

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,20 +5,7 @@ module mips.emulator {
 
     exports GUI;
     opens GUI to javafx.fxml;
-    exports GUI.temp.misc;
-    opens GUI.temp.misc to javafx.fxml;
-    exports GUI.temp.tabs;
-    opens GUI.temp.tabs to javafx.fxml;
-    exports GUI.temp.menu;
-    opens GUI.temp.menu to javafx.fxml;
-    exports GUI.temp.display;
-    opens GUI.temp.display to javafx.fxml;
-    exports GUI.temp.accelerometer;
-    opens GUI.temp.accelerometer to javafx.fxml;
 
     // Required for project.fxml to be able to use Controller
     opens controller to javafx.fxml;
-    exports GUI.temp;
-    opens GUI.temp to javafx.fxml;
-    opens controller.temp to javafx.fxml;
 }
