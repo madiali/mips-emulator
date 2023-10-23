daaad66cdb8732b0a4d5ff1771dcd54825848ca1
Jesse Wei <jesse@cs.unc.edu>
2023-10-23 19:09:19 -0400

    curl: use --remote-name, alias mips_em -> mips-em

--- src/main/sh/install.sh
+++ src/main/sh/install.sh
@@ -4,10 +4,10 @@ INSTALL_DIR=$HOME/.mips-emulator
 MIPS_EMULATOR=https://github.com/madiali/mips-emulator/releases/latest/download/mips-emulator-1.0.0-with-dependencies.jar
 # Must be a version with fx bundled
 JAVA_VER="17.0.9.fx-librca"
-# Everything after final slash in URL, don't change
+# String after final slash in URL
 JAR_NAME="${MIPS_EMULATOR##*/}"
 
-# Install Java 17 + FX
+# Install SDKMAN!
 curl -s "https://get.sdkman.io" | bash
 source $HOME/.sdkman/bin/sdkman-init.sh
 # sdkman asks whether to set the installed java version
@@ -16,10 +16,12 @@ yes | sdk install java $JAVA_VER
 # Make sure it's the default version
 sdk default java $JAVA_VER
 
-curl -OLH "Accept: application/zip" $MIPS_EMULATOR --create-dirs --output-dir $INSTALL_DIR
+mkdir -p $INSTALL_DIR
+curl -OLH "Accept: application/zip" --create-dirs --remote-name $MIPS_EMULATOR
+mv $JAR_NAME $INSTALL_DIR
 
 # Determine name of shell rc file
-# This script will always be run via bash
+# This script is always run via bash
 # But $SHELL will be user's shell (e.g., zsh)
 # Even if run via bash
 
@@ -37,9 +39,10 @@ Otherwise, the .jar has already been downloaded and can be run however you norma
 fi
 
 # Alias the command so user can start MIPS Emulator from any directory
-echo "alias mips_em=\"java -jar $INSTALL_DIR/$JAR_NAME\"" >> "$SHELL_RC_FILE"
+echo "alias mips-em=\"java -jar $INSTALL_DIR/$JAR_NAME\"" >> "$SHELL_RC_FILE"
 printf "$JAR_NAME has been downloaded to $INSTALL_DIR/$JAR_NAME,
-and the command \`mips_em\` has been aliased in your $SHELL_RC_FILE file.
+and the command \`mips-em\` has been aliased in your $SHELL_RC_FILE file.
 Run \`source $SHELL_RC_FILE\` to apply changes to your current shell session. Otherwise, restart your terminal.
-Then run \`mips_em\` to start MIPS Emulator.
+Then run \`mips-em\` to start MIPS Emulator.
+For usage details, please read https://github.com/madiali/mips-emulator#usage and/or https://comp541.web.unc.edu.
 Enjoy!\n"
