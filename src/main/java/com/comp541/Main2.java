ebcc99fa5fd50c18d620b684ae6bbca8738172ef
Jesse Wei <jesse@cs.unc.edu>
2023-11-26 17:20:53 -0500

    Working sound

---
+++ src/main/java/com/comp541/Main2.java
@@ -0,0 +1,10 @@
+package com.comp541;
+
+import org.jfugue.player.Player;
+
+public class Main2 {
+    public static void main(String[] args) {
+        Player player = new Player();
+        player.play("C D E F G A B");
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module mips.emulator {
     requires javafx.controls;
     requires javafx.fxml;
     requires org.json;
+    requires jfugue;
     exports com.comp541.GUI;
     opens com.comp541.GUI to javafx.fxml;
     exports com.comp541;
